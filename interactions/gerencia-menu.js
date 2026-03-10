dbc2109c562fd87dd9d2aa8fc596aab8aea8c9d9
Lucas Souza <www.luucas@gmail.com>
2026-03-10 17:47:53 -0300

    add cancel button on gerencia

--- interactions/gerencia-menu.js
+++ interactions/gerencia-menu.js
@@ -1,14 +1,51 @@
 const { ActionRowBuilder, StringSelectMenuBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
-const { embedGerencia } = require('../utils/embeds');
+const { embedGerencia, embedMenuPrincipal } = require('../utils/embeds');
 const { getCategorias } = require('../utils/db');
+
+function rowMenuGerencia() {
+  return new ActionRowBuilder().addComponents(
+    new ButtonBuilder()
+      .setCustomId('ger_adicionar_item')
+      .setLabel('➕ Adicionar Item')
+      .setStyle(ButtonStyle.Success),
+    new ButtonBuilder()
+      .setCustomId('ger_remover_item')
+      .setLabel('➖ Remover Item')
+      .setStyle(ButtonStyle.Danger),
+    new ButtonBuilder()
+      .setCustomId('ger_zerar_bau')
+      .setLabel('🗑️ Zerar Baú')
+      .setStyle(ButtonStyle.Secondary),
+    new ButtonBuilder()
+      .setCustomId('ger_ver_logs')
+      .setLabel('📋 Ver Logs')
+      .setStyle(ButtonStyle.Primary)
+  );
+}
+
+function rowCancelar() {
+  return new ActionRowBuilder().addComponents(
+    new ButtonBuilder()
+      .setCustomId('ger_cancelar')
+      .setLabel('❌ Cancelar')
+      .setStyle(ButtonStyle.Secondary)
+  );
+}
 
 module.exports = {
   type: 'button',
-  customIds: ['ger_adicionar_item', 'ger_remover_item', 'ger_zerar_bau', 'ger_ver_logs'],
+  customIds: ['ger_adicionar_item', 'ger_remover_item', 'ger_zerar_bau', 'ger_ver_logs', 'ger_cancelar'],
 
   async execute(interaction, client) {
     await interaction.deferUpdate();
     const acao = interaction.customId;
+
+    if (acao === 'ger_cancelar') {
+      return interaction.editReply({
+        embeds: [embedGerencia()],
+        components: [rowMenuGerencia()]
+      });
+    }
 
     if (acao === 'ger_adicionar_item') {
       const categorias = await getCategorias();
@@ -19,7 +56,7 @@ module.exports = {
 
       return interaction.editReply({
         embeds: [embedGerencia().setDescription('> Selecione a categoria do novo item:')],
-        components: [new ActionRowBuilder().addComponents(select)]
+        components: [new ActionRowBuilder().addComponents(select), rowCancelar()]
       });
     }
 
@@ -32,7 +69,7 @@ module.exports = {
 
       return interaction.editReply({
         embeds: [embedGerencia().setDescription('> Selecione a categoria do item que deseja remover:')],
-        components: [new ActionRowBuilder().addComponents(select)]
+        components: [new ActionRowBuilder().addComponents(select), rowCancelar()]
       });
     }
 
@@ -48,7 +85,7 @@ module.exports = {
 
       return interaction.editReply({
         embeds: [embedGerencia().setDescription('> ⚠️ Selecione qual baú deseja zerar. Essa ação é irreversível!')],
-        components: [new ActionRowBuilder().addComponents(select)]
+        components: [new ActionRowBuilder().addComponents(select), rowCancelar()]
       });
     }
 
@@ -63,7 +100,7 @@ module.exports = {
 
       return interaction.editReply({
         embeds: [embedGerencia().setDescription('> Selecione de qual baú deseja ver os logs:')],
-        components: [new ActionRowBuilder().addComponents(select)]
+        components: [new ActionRowBuilder().addComponents(select), rowCancelar()]
       });
     }
   }
